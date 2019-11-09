b7f00dfd0cfb60425fb4b2574a992d1b94a365f9
ferreiraluana <ljnf@ic.ufal.br>
2019-11-09 17:19:51 -0300

    corrigindo o view

--- blog/views.py
+++ blog/views.py
@@ -10,5 +10,5 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
